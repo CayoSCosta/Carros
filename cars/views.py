0e5327ddb376f28d8062828b764e6eb5521e2e45
Cayo Costa <cayo.costa@outlook.com>
2024-10-28 14:23:46 -0300

    fotos dos carros e melhorias no html

--- cars/views.py
+++ cars/views.py
@@ -2,8 +2,18 @@ from django.shortcuts import render
 from cars.models import Car
 
 def cars_view(request):
-    cars = Car.objects.all()
+    search = request.GET.get('search')
+
+    try:
+        if search:
+            cars = Car.objects.filter(model__icontains=search)
+        else:
+            cars = Car.objects.all()
+    except Exception as e:
+        return render(request, 'cars.html', {'error': str(e)})
+    
     # cars = Car.objects.filter(brand__name='Hyundai') # Fiat
     # cars = Car.objects.filter(model='Palio')21111
     # cars = Car.objects.filter(brand__name__contains='i')
+    
     return render(request, 'cars.html', {'cars': cars}) 
